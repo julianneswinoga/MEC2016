e4fb1201561c6212988d40094e0f29b2bd9edf6f
Cameron Swinoga <swinogca@mcmaster.ca>
2016-10-22 16:36:32 -0400

    Class updates

--- control.py
+++ control.py
@@ -1,11 +1,19 @@
 # Control module
+#from powermanager import powermanager
 
 class control:
 	def __init__(self):
-		pass
+		pass#self.sensors_module = powermanager()
 	
+	def status(self, **kwargs): # module
+		if ('module' not in kwargs or 'function' not in kwargs):
+			print 'Function status called wrong'
+			return False
 		
+			
+			
 if __name__ == "__main__":
-	cntrl = CONTROL()
-	print "Control module"
+	cntrl = control()
+	print 'Control module'
+	cntrl.status(module = cntrl, function = 'temp')
 	
