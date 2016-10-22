a2fc756a277174fed0ffa759c8072c9716924700
laruepa <laruepa@mcmaster.ca>
2016-10-22 17:21:30 -0400

    Added get sensor data functionality to sensor class

--- sensors/src/sensors.py
+++ sensors/src/sensors.py
@@ -11,8 +11,16 @@ class Sensors:
         self.barometer
     
     def readSensors(self):
+        #Read values into local variables from individual sensor classes
         self.accelerometer = accelerometer.getAccelData()
         self.waterTemperature = temperature.getWaterTemp()
         self.airTemperature = temperature.getAirTemp()
         self.barometer = barometer.getPressure()
+        
+        #Format output to control in string var temp
+        temp = ("Accelerometer: "+self.accelerometer+"G's"
+                +"\nWater Temperature: "+self.waterTemperature+"degC"
+                +"\nAir Temperature: "+self.airTemperature+"degC"
+                +"\nBarometric Pressure: "+self.barometer+"kPa")
+        return temp;
     #end
