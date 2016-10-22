775adbff3b19a5a44822d040e585d688c3e480c7
laruepa <laruepa@mcmaster.ca>
2016-10-22 17:14:18 -0400

    Added peripheral sensor support for sensor.py class

--- sensors/src/sensors.py
+++ sensors/src/sensors.py
@@ -1,11 +1,18 @@
+import accelerometer.py
+import temperature.py
+import barometer.py
+
 class Sensors:
     
     def __init__(self):
         self.accelerometer
-        self.temperature
+        self.waterTemperature
+        self.airTemperature
         self.barometer
     
     def readSensors(self):
-        """"""
-
+        self.accelerometer = accelerometer.getAccelData()
+        self.waterTemperature = temperature.getWaterTemp()
+        self.airTemperature = temperature.getAirTemp()
+        self.barometer = barometer.getPressure()
     #end
