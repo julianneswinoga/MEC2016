06b2700247c1d1362db70f3f4ea1fe12069e7fcb
laruepa <laruepa@mcmaster.ca>
2016-10-22 19:37:07 -0400

    added light sensor class

---
+++ lightsensor.py
@@ -0,0 +1,12 @@
+class LightSensor:
+
+	def __init__(self):
+		self.lightsensor = 0
+		self.updateLightData()
+
+	def getLightData(self):
+		self.updateLightData()
+		return self.lightsensor
+
+	def updateLightData(self):
+		self.lightsensor = 35
