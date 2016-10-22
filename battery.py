54f2e15c5bfc19561dd61ec8d8fc8b70597f404b
Aleks Mercik <aleks.mercik@outlook.com>
2016-10-22 17:00:33 -0400

    Work on power manager

--- battery.py
+++ battery.py
@@ -1,7 +1,10 @@
-class battery:
-    
+class Battery:
     def __init__(self):
+        self.voltage = 12
         return None
         
     def getStatus(self):
         return True
+        
+    def getVoltage(self):
+        return self.voltage
