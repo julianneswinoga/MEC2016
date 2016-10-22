5bb849699c971e1da25ce9f16f7fc9672c334564
Julianne Swinoga <julianneswinoga@gmail.com>
2016-10-22 17:00:56 -0400

    Get status implemented

--- control.py
+++ control.py
@@ -1,19 +1,23 @@
 # Control module
-#from powermanager import powermanager
+import sys
+from powermanager import powermanager
 
-class control:
+class Control:
 	def __init__(self):
-		pass#self.sensors_module = powermanager()
+		self.sensors_module = powermanager()
 	
 	def status(self, **kwargs): # module
-		if ('module' not in kwargs or 'function' not in kwargs):
+		if ('module' not in kwargs or 'function' not in kwargs or 'args' not in kwargs):
 			print 'Function status called wrong'
 			return False
+		if (not kwargs['module'].getStatus()):
+			return {'err': False, 'msg': 'Status of ' + kwargs['module'].__name__ + ' has failed'}
 		
+		return getattr(kwargs['module'], kwargs['function'])(*kwargs['args'])
 			
 			
 if __name__ == "__main__":
-	cntrl = control()
-	print 'Control module'
-	cntrl.status(module = cntrl, function = 'temp')
+	cntrl = Control()
+	print 'Control module start'
+	print cntrl.status(module = cntrl.sensors_module, function = 'getStatus', args = [])
 	
