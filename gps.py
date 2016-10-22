9411e170fea4b219c99833f83d7236112b93fbbe
laruepa <laruepa@mcmaster.ca>
2016-10-22 19:27:10 -0400

    added gps.py

---
+++ gps.py
@@ -0,0 +1,16 @@
+class GPS:
+
+	def __init__(self):
+		self.coordN = 0.000
+		self.coordW = 0.000
+	
+	def getGPSData(self):
+		self.updateGPSData()
+		g = []
+		g.append(self.coordN)
+		g.append(self.coordW)
+		return g
+
+	def updateGPSData(self):
+		self.coordN = 40.446
+		self.coordW = 79.982
