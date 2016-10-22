a74e37b051eba19bceb131300236a8fef5ff9b39
Aleks Mercik <aleks.mercik@outlook.com>
2016-10-22 17:39:11 -0400

    Added comments to battery and solar panel. added random variables for simulation

--- battery.py
+++ battery.py
@@ -1,10 +1,13 @@
-class Battery:
-    def __init__(self):
-        self.batteryVoltage = 12
+import random #for simulation
+
+class Battery: #battery
+    def __init__(self): #battey initialization
+        self.batteryVoltage = 11.5+random.random()
         return None
         
-    def getStatus(self):
-        return True
+    def getStatus(self): #return the status
+        return True        
         
-    def getVoltage(self):
+    def getVoltage(self): #return the battery voltage
+        self.batteryVoltage = 11.5+random.random()
         return self.batteryVoltage
