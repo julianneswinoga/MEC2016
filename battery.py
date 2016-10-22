6c35ae94937066ce89789c31dfb046cc59d21038
Aleks Mercik <aleks.mercik@outlook.com>
2016-10-22 17:27:53 -0400

    Added solar panel

--- battery.py
+++ battery.py
@@ -1,10 +1,10 @@
 class Battery:
     def __init__(self):
-        self.systemVoltage = 12
+        self.batteryVoltage = 12
         return None
         
     def getStatus(self):
         return True
         
     def getVoltage(self):
-        return self.systemVoltage
+        return self.batteryVoltage
