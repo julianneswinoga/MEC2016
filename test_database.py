836d324307f7247901ad704eb9a677cf4dcf4561
francium <varunhooda1@gmail.com>
2016-10-22 18:55:52 -0400

    range query is broken

--- test_database.py
+++ test_database.py
@@ -3,4 +3,11 @@ from database import Database as DB
 db = DB()
 
 db.insert({'light': 0, 'acc': 5.0, 'tmp': 10.0, 'baro': 999.0})
-db.insert({'light': 1, 'acc': 5.0, 'tmp': 10.0, 'baro': 100.0})
+db.insert({'light': 'null', 'acc': 5.0, 'tmp': 10.0, 'baro': 100.0})
+print db.retrieve('all')
+print
+
+print db.retrieve('range', '2010-10-01', '2016-11-31')
+print
+
+print db.retrieve('latest')
