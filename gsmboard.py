7de7961681f0331e36dbd3e67855d07eea774908
francium <varunhooda1@gmail.com>
2016-10-22 20:04:26 -0400

    Server and send function

--- gsmboard.py
+++ gsmboard.py
@@ -1,13 +1,32 @@
+import Server
 
 class GsmBoard: #gsm board module
     def __init__(self): #initialization
         self.status = True
         
         self.buffer = [] #internal command buffer
+
+        self.server = Server(self)
+
+        self.PORT = 8888;
         
     def getStatus(self): #get the component status
         return self.status
         
+    def send(host, msg):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except socket.error:
+            print 'Failed to create socket'
+            sys.exit(1)
+         
+        try :
+            #Set the whole string
+            s.sendto(msg, (host, self.PORT))
+        except socket.error, msg:
+            print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit(1)
+
     def isPending(self): #check if there is an element in the buffer
         return bool(len(self.buffer))        
         
